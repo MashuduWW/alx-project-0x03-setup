76779fd8b1c8c0e9e27d838261ffd92ab1b63595
Mashudu Molema <mrkaymp@gmail.com>
2025-07-25 09:36:59 +0200

    Renamed directory interface

--- alx-project-0x03/components/common/Button.tsx
+++ alx-project-0x03/components/common/Button.tsx
@@ -1,12 +1,19 @@
-import { ButtonProps } from "@/interfaces";
+import { ButtonProps } from "@/interface";
 
-const Button = ({ buttonLabel, buttonSize, buttonBackgroundColor, action }: ButtonProps) => {
-  const backgroundColorClass = buttonBackgroundColor ? {
-    red: 'bg-red-500',
-    blue: 'bg-blue-500',
-    orange: 'bg-orange-500',
-    green: 'bg-green-500',
-  }[buttonBackgroundColor] : 'bg-slate-500';
+const Button = ({
+  buttonLabel,
+  buttonSize,
+  buttonBackgroundColor,
+  action,
+}: ButtonProps) => {
+  const backgroundColorClass = buttonBackgroundColor
+    ? {
+        red: "bg-red-500",
+        blue: "bg-blue-500",
+        orange: "bg-orange-500",
+        green: "bg-green-500",
+      }[buttonBackgroundColor]
+    : "bg-slate-500";
 
   return (
     <button
--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,6 +1,6 @@
 import Button from "@/components/common/Button";
 import { useRouter } from "next/router";
-import { PageRouteProps } from "@/interfaces";
+import { PageRouteProps } from "@/interface";
 
 export default function Home() {
   const router = useRouter();
@@ -20,9 +20,21 @@ export default function Home() {
       </p>
 
       <div className="flex gap-6">
-        <Button action={() => routeToNextPage({ pageRoute: '/generate-text-ai' })} buttonLabel="Generate Text" buttonBackgroundColor="blue" />
-        <Button action={() => routeToNextPage({ pageRoute: '/text-to-image' })} buttonLabel="Text to Image" buttonBackgroundColor="green" />
-        <Button action={() => routeToNextPage({ pageRoute: '/counter-app' })} buttonLabel="Contact us" buttonBackgroundColor="orange" />
+        <Button
+          action={() => routeToNextPage({ pageRoute: "/generate-text-ai" })}
+          buttonLabel="Generate Text"
+          buttonBackgroundColor="blue"
+        />
+        <Button
+          action={() => routeToNextPage({ pageRoute: "/text-to-image" })}
+          buttonLabel="Text to Image"
+          buttonBackgroundColor="green"
+        />
+        <Button
+          action={() => routeToNextPage({ pageRoute: "/counter-app" })}
+          buttonLabel="Contact us"
+          buttonBackgroundColor="orange"
+        />
       </div>
     </div>
   );
